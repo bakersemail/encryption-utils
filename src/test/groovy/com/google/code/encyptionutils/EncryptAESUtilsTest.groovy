3168528e1a989cfcc981c741da20e08a0b80197e
baker.steven.83@gmail.com <baker.steven.83@gmail.com@116e2cb2-40a5-35ee-fdc4-6782d269f154>
2011-06-28 23:55:17 +0000

    initial checkin. moved from private svn to google code.

---
+++ src/test/groovy/com/google/code/encyptionutils/EncryptAESUtilsTest.groovy
@@ -0,0 +1,35 @@
+package com.google.code.encyptionutils
+
+import org.junit.Test
+
+class EncryptAESUtilsTest {
+	static final String INPUT = "some input"
+	static final ENCODING_STRATEGY = new Base32Encoding()
+	
+	EncryptUtils utils = new EncryptAESUtils(ENCODING_STRATEGY, "some key")
+	
+	@Test
+	void shouldEncryptAndDecryptToSameInput() {
+		def encrypted = utils.encrypt(INPUT)
+		def decrypted = utils.decrypt(encrypted)
+		
+		assert decrypted == INPUT
+	}
+	
+	@Test
+	void shouldEncryptAndDecryptEncodedToSameInput() {
+		def encrypted = utils.encryptAndEncode(INPUT)
+		assert encrypted
+
+		String decrypted = utils.decryptEncoded(encrypted)
+		assert INPUT == decrypted
+	}
+	
+	@Test
+	void shouldEncodeAndDecodeToSameInput() {
+		String encoded = utils.encode(INPUT.getBytes())
+		
+		String decoded = new String(utils.decode(encoded))
+		assert decoded == INPUT
+	}
+}
