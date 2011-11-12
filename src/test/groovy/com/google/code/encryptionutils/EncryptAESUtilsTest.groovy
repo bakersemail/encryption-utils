391d71d80ab9769d04f3899e11e3f1d2faff1507
baker.steven.83@gmail.com <baker.steven.83@gmail.com@116e2cb2-40a5-35ee-fdc4-6782d269f154>
2011-11-12 01:56:20 +0000

    just code coverage

--- src/test/groovy/com/google/code/encryptionutils/EncryptAESUtilsTest.groovy
+++ src/test/groovy/com/google/code/encryptionutils/EncryptAESUtilsTest.groovy
@@ -3,10 +3,30 @@ package com.google.code.encryptionutils
 import org.junit.Test
 
 class EncryptAESUtilsTest {
-	static final String INPUT = "some input"
+	static final String INPUT = 'some input'
 	static final ENCODING_STRATEGY = new Base32Encoding()
 	
-	EncryptUtils utils = new EncryptAESUtils(ENCODING_STRATEGY, "some key")
+	EncryptUtils utils = new EncryptAESUtils(ENCODING_STRATEGY, 'some key')
+	
+	@Test(expected = AssertionError)
+	void shouldThrowExceptionWhenNoStratProvided() {
+		new EncryptAESUtils(null, null)
+	}
+	
+	@Test(expected = RuntimeException)
+	void shouldThrowExceptionWhenNullKey() {
+		new EncryptAESUtils(ENCODING_STRATEGY, null)
+	}
+	
+	@Test(expected = RuntimeException)
+	void shouldThrowExceptionWhenEncryptingNull() {
+		utils.encrypt(null)
+	}
+	
+	@Test(expected = RuntimeException)
+	void shouldThrowExceptionWhenDecryptingNull() {
+		utils.decrypt(null)
+	}
 	
 	@Test
 	void shouldEncryptAndDecryptToSameInput() {
